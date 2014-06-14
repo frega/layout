45965e5fb9cebc595f6331acbebfb5b336e8ce64
frega <fl@flink-solutions.de>
2014-06-14 17:52:38 +0200

    Add sortHelper to render plugin

--- src/Plugin/LayoutRegion/LayoutRegionPluginBag.php
+++ src/Plugin/LayoutRegion/LayoutRegionPluginBag.php
@@ -14,4 +14,17 @@ use Drupal\Core\Plugin\DefaultPluginBag;
  */
 class LayoutRegionPluginBag extends DefaultPluginBag {
   // @todo: implement sortHelper for configurable and non-configurable LayoutRegion plugins.
+  /**
+   * {@inheritdoc}
+   */
+  public function sortHelper($aID, $bID) {
+    $a_weight = $this->get($aID)->getWeight();
+    $b_weight = $this->get($bID)->getWeight();
+    if ($a_weight == $b_weight) {
+      return 0;
+    }
+
+    return ($a_weight < $b_weight) ? -1 : 1;
+  }
+
 }
